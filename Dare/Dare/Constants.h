3518ece4ce874fe28c121f1a28208a79926c5db7
Dare Ryan <dareryan@me.com>
2014-06-28 09:47:43 -0400

    commit project

---
+++ Dare/Dare/Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  Dare
+//
+//  Created by Dare Ryan on 6/25/14.
+//  Copyright (c) 2014 Dare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+extern NSString *const ParseAppID;
+extern NSString *const ParseClientKey;
+
+@end
---
+++ Dare/Dare/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dare
+//
+//  Created by Dare Ryan on 6/25/14.
+//  Copyright (c) 2014 Dare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

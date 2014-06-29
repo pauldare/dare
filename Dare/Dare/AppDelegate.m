ce4d6b94f179c89ea1f1b8abe02591673e83d793
Carlos Meirin <carlos@Carloss-15-MBP.local>
2014-06-28 21:41:18 -0400

    Add login VC

--- Dare/Dare/AppDelegate.m
+++ Dare/Dare/AppDelegate.m
@@ -111,7 +111,6 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Saves changes in the application's managed object context before the application terminates.
-    [self saveContext];
 }
 
 

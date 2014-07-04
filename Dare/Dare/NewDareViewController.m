22eb4e50e28a500a6834166faf4dd97e2603bdb8
= <=>
2014-07-04 19:29:44 -0400

    Start work on NewDareVC

--- Dare/Dare/NewDareViewController.m
+++ Dare/Dare/NewDareViewController.m
@@ -13,6 +13,7 @@
 #import "FriendListIcon.h"
 #import "SelectDareCell.h"
 #import "ParseClient.h"
+#import <FontAwesomeKit/FontAwesomeKit.h>
 
 
 @interface NewDareViewController () <UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDataSource>
@@ -20,6 +21,8 @@
 @property (weak, nonatomic) IBOutlet UIView *cameraView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (strong, nonatomic) CameraManager *cameraManager;
+@property (weak, nonatomic) IBOutlet UIButton *albumButton;
+@property (weak, nonatomic) IBOutlet UIButton *flipButton;
 
 @property (weak, nonatomic) IBOutlet UICollectionView *friendsCollection;
 
@@ -29,9 +32,10 @@
 @property (strong, nonatomic) NSArray *images;
 @property (strong, nonatomic) NSArray *messages;
 //@property (strong, nonatomic) NSArray *friends;
-@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
 @property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
 @property (strong, nonatomic) UIView *dareTextImageOverlay;
+@property (strong, nonatomic) UILabel *takeAPhotoOverlay;
 
 
 
@@ -45,30 +49,62 @@
 {
     [super viewDidLoad];
     
+    
     self.images = @[[UIImage imageNamed:@"angry.jpeg"], [UIImage imageNamed:@"tricolor.jpeg"], [UIImage imageNamed:@"kitten.jpeg"], [UIImage imageNamed:@"cat.jpeg"]];
     self.messages = @[@"I DARE YOU\nto pet a cat", @"I DARE YOU\nto eat icecream", @"I DARE YOU\nto have fun"];
     
-    _imageView.backgroundColor = [UIColor DareBlue];
-    _cameraView.backgroundColor = [UIColor DareBlue];
+    _imageView.backgroundColor = [UIColor whiteColor];
+    _cameraView.backgroundColor = [UIColor whiteColor];
     self.imageView.hidden = YES;
     
     [self.view bringSubviewToFront:self.cameraButton];
+    [self.view bringSubviewToFront:self.flipButton];
+    [self.view bringSubviewToFront:self.albumButton];
+    [_cameraButton setTintColor:[UIColor DareBlue]];
+    [_flipButton setTintColor:[UIColor DareBlue]];
+    [_albumButton setTintColor:[UIColor DareBlue]];
+    
+    FAKFontAwesome *cameraIcon = [FAKFontAwesome cameraIconWithSize:60];
+    [cameraIcon addAttribute:NSForegroundColorAttributeName value:[UIColor DareBlue]];
+    UIImage *cameraImage = [cameraIcon imageWithSize:CGSizeMake(60, 60)];
+    [_cameraButton setTitle:@"" forState:UIControlStateNormal];
+    [_cameraButton setImage:cameraImage forState:UIControlStateNormal];
+    
+    FAKFontAwesome *flipCameraIcon = [FAKFontAwesome refreshIconWithSize:60];
+    [flipCameraIcon addAttribute:NSForegroundColorAttributeName value:[UIColor DareBlue]];
+    UIImage *flipImage = [flipCameraIcon imageWithSize:CGSizeMake(60, 60)];
+    [_flipButton setTitle:@"" forState:UIControlStateNormal];
+    [_flipButton setImage:flipImage forState:UIControlStateNormal];
+    
+    FAKFontAwesome *existingPhotoIcon = [FAKFontAwesome photoIconWithSize:60];
+    [existingPhotoIcon addAttribute:NSForegroundColorAttributeName value:[UIColor DareBlue]];
+    UIImage *existingPhotoImage = [existingPhotoIcon imageWithSize:CGSizeMake(60, 60)];
+    [_albumButton setTitle:@"" forState:UIControlStateNormal];
+    [_albumButton setImage:existingPhotoImage forState:UIControlStateNormal];
+    
+    FAKFontAwesome *rightLabel = [FAKFontAwesome forwardIconWithSize:40];
+    [rightLabel addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor]];
+    UIImage *rightLabelImage = [rightLabel imageWithSize:CGSizeMake(40, 40)];
+    [_forwardButton setTitle:@"" forState:UIControlStateNormal];
+    [_forwardButton setTintColor:[UIColor whiteColor]];
+    [_forwardButton setImage:rightLabelImage forState:UIControlStateNormal];
+    
+    FAKFontAwesome *leftLabel = [FAKFontAwesome backwardIconWithSize:40];
+    [leftLabel addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor]];
+    UIImage *leftLabelImage = [leftLabel imageWithSize:CGSizeMake(40, 40)];
+    [_backButton setTitle:@"" forState:UIControlStateNormal];
+    [_backButton setTintColor:[UIColor whiteColor]];
+    [_backButton setImage:leftLabelImage forState:UIControlStateNormal];
+    
+    
     [self setupCamera];
     [self setupFriendsCollection];
     [self setupTextCollection];
     
-   // _dareTextImageOverlay = [[UIView alloc]initWithFrame:CGRectMake(0, _imageView.center.y, _imageView.frame.size.width, 80)];
-    _dareTextImageOverlay = [[UIView alloc]initWithFrame:CGRectMake(0,self.view.frame.size.height, _imageView.frame.size.width, 80)];
-    _dareTextImageOverlay.backgroundColor = [UIColor redColor];
-    [self.view addSubview:_dareTextImageOverlay];
-    [self performSelector:@selector(moveOverlayIntoView) withObject:self afterDelay:3.0];
     
-    UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
-    [_dareTextImageOverlay addGestureRecognizer:panRecognizer];
+    [self setupImageOverlay];
     
-    UITapGestureRecognizer *tapOnImageOverlay = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(cropImageToOverlay)];
-    tapOnImageOverlay.numberOfTapsRequired = 1;
-    [_dareTextImageOverlay addGestureRecognizer:tapOnImageOverlay];
+    
     
     //    [ParseClient getUser:[PFUser currentUser] completion:^(User *loggedUser) {
     //        self.friends = [[NSMutableArray alloc]initWithObjects:[PFUser currentUser], nil];
@@ -94,12 +130,45 @@
     [self.view bringSubviewToFront:self.forwardButton];
     [self.view bringSubviewToFront:self.backButton];
 }
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    _takeAPhotoOverlay = [[UILabel alloc]initWithFrame:_textCollection.frame];
+    _takeAPhotoOverlay.backgroundColor = [UIColor DareBlue];
+    _takeAPhotoOverlay.textColor = [UIColor whiteColor];
+    _takeAPhotoOverlay.text = @"Take a photo or choose one";
+    _takeAPhotoOverlay.font = [UIFont boldSystemFontOfSize:20];
+    _takeAPhotoOverlay.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:_takeAPhotoOverlay];
+    [self.view bringSubviewToFront:_takeAPhotoOverlay];
+    _textCollection.hidden = YES;
+    _backButton.hidden = YES;
+    _forwardButton.hidden = YES;
+}
+
+-
+
+-(void)setupImageOverlay
+{
+    _dareTextImageOverlay = [[UIView alloc]initWithFrame:CGRectMake(0,self.view.frame.size.height, _imageView.frame.size.width, 80)];
+    _dareTextImageOverlay.backgroundColor = [UIColor DareCellOverlay];
+    [self.view addSubview:_dareTextImageOverlay];
+    //[self performSelector:@selector(moveOverlayIntoView) withObject:self afterDelay:3.0];
+    UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    [_dareTextImageOverlay addGestureRecognizer:panRecognizer];
+    
+    UITapGestureRecognizer *tapOnImageOverlay = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(cropImageToOverlay)];
+    tapOnImageOverlay.numberOfTapsRequired = 1;
+    [_dareTextImageOverlay addGestureRecognizer:tapOnImageOverlay];
+
+
+}
 
 -(void)moveOverlayIntoView
 {
     [UIView animateWithDuration:0.3 animations:^{
         
-        [_dareTextImageOverlay setFrame:CGRectMake(0, _cameraView.center.y, _cameraView.frame.size.width, 80)];
+        [_dareTextImageOverlay setFrame:CGRectMake(0, _cameraView.frame.origin.y, _cameraView.frame.size.width, 112)];
     }];
 }
 
@@ -118,31 +187,35 @@
 
     CGPoint translation = [uigr translationInView:_dareTextImageOverlay.superview];
     
-    if (_dareTextImageOverlay.frame.origin.y <= _cameraView.frame.origin.y && translation.y < 0) {
-    
-        return;
+    if (_dareTextImageOverlay.frame.origin.y <= _cameraView.frame.origin.y  && translation.y < 0) {
+       
+        [UIView animateWithDuration:0.3 animations:^{
+            _dareTextImageOverlay.frame = CGRectMake(0, _cameraView.frame.origin.y, _dareTextImageOverlay.frame.size.width, _dareTextImageOverlay.frame.size.height);
+        }];
     }else if (CGRectGetMaxY(_dareTextImageOverlay.frame) >= CGRectGetMaxY(_cameraView.frame) && translation.y > 0){
-        return;
-    }
+        
+        [UIView animateWithDuration:0.3 animations:^{
+            _dareTextImageOverlay.frame = CGRectMake(0, _cameraView.frame.origin.y + (_cameraView.frame.size.height - _dareTextImageOverlay.frame.size.height), _dareTextImageOverlay.frame.size.width, _dareTextImageOverlay.frame.size.height);
+        }];
+    }else{
     _dareTextImageOverlay.center = CGPointMake(_imageView.center.x,
                                                _lastLocation.y + translation.y);
+    }
 }
 
 - (UIImage *)cropImageToOverlay
 {
     _dareTextImageOverlay.hidden = YES;
-    //CGRect CropRect = CGRectMake(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height+15);
     UIGraphicsBeginImageContext(self.view.frame.size);
     [self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
     UIImage *fullScreenshot = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-
-    
     CGImageRef imageRef = CGImageCreateWithImageInRect([fullScreenshot CGImage], _dareTextImageOverlay.frame);
     UIImage *cropped = [UIImage imageWithCGImage:imageRef];
     CGImageRelease(imageRef);
     
     _dareTextImageOverlay.hidden = NO;
+    UIImageWriteToSavedPhotosAlbum(cropped, nil, nil, nil);
     return cropped;
 }
 - (IBAction)forwardButtonPressed:(id)sender
@@ -209,6 +282,8 @@
                                         } failure:^{
                                             [self selectPictureFromLibrary];
                                         }];
+    
+    _takeAPhotoOverlay.text = @"Tap here to use this image";
 }
 
 
@@ -237,7 +312,7 @@
     if (collectionView == self.friendsCollection) {
         return [self.images count];
     } else {
-        return [self.messages count];
+        return [self.messages count] + 1;
     }
 }
 
@@ -273,7 +348,14 @@
         return cell;
     } else {
         SelectDareCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"SelectDareCell" forIndexPath:indexPath];
-        ((SelectDareCell *)cell).messageLabel.text = self.messages[indexPath.row];
+        ((SelectDareCell *)cell).messageLabel.textColor = [UIColor whiteColor];
+        ((SelectDareCell *)cell).messageLabel.font = [UIFont boldSystemFontOfSize:20];
+       
+        if (indexPath.row == 0) {
+            ((SelectDareCell *)cell).messageLabel.text = @"I DARE YOU TO...\ncreate your own";
+        }else{
+        ((SelectDareCell *)cell).messageLabel.text = self.messages[indexPath.row - 1];
+        }
         return cell;
     }
 }

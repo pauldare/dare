a6c2807311acbca0e956501f15de3e078df05f72
Nadia <joystate@me.com>
2014-07-05 13:57:36 -0400

    snapcomment view controller

---
+++ Dare/Dare/SnapCommentVC.h
@@ -0,0 +1,20 @@
+//
+//  SnapCommentVC.h
+//  Dare
+//
+//  Created by Nadia on 7/5/14.
+//  Copyright (c) 2014 Dare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SnapCommentVC : UIViewController
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UIImageView *dareImageView;
+@property (weak, nonatomic) IBOutlet UIView *dareView;
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@property (weak, nonatomic) IBOutlet UIView *cameraView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+
+@end

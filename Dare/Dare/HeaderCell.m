40e9fe2b43034f3dd94c552b51d0ee22e33bf5c3
Nadia <joystate@me.com>
2014-07-04 22:27:49 -0400

    add messages view controller

---
+++ Dare/Dare/HeaderCell.m
@@ -0,0 +1,80 @@
+//
+//  HeaderCell.m
+//  Dare
+//
+//  Created by Nadia on 7/4/14.
+//  Copyright (c) 2014 Dare. All rights reserved.
+//
+
+#import "HeaderCell.h"
+#import "FriendListIcon.h"
+#import "Friend.h"
+
+
+@implementation HeaderCell
+
+- (void)awakeFromNib
+{
+    self.dataStore = [DareDataStore sharedDataStore];
+    [self fetchFriends:^{
+        [self.collectionView reloadData];
+    }];
+    UINib *friendNib = [UINib nibWithNibName:@"FriendListIcon" bundle:nil];
+    [self.collectionView registerNib:friendNib forCellWithReuseIdentifier:@"FriendCell"];
+}
+
+- (void)fetchFriends: (void(^)())completion
+{
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:@"Friend"];
+    self.friends = [self.dataStore.managedObjectContext executeFetchRequest:fetchRequest error:nil];
+    completion();
+}
+
+
+#pragma collection view delegate
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return [self.friends count];
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    CGFloat oneThirdOfDisplay = self.collectionView.frame.size.width/3;
+    return CGSizeMake(oneThirdOfDisplay, collectionView.frame.size.height);
+}
+
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+{
+    return -1.0;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
+{
+    return -1.0;
+}
+
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    FriendListIcon *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"FriendCell" forIndexPath:indexPath];
+    Friend *friend = self.friends[indexPath.row];
+    UIImage *image = [UIImage imageWithData:friend.image];
+    ((FriendListIcon*)cell).friendImage.image = image;
+    return cell;
+}
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
--- Dare/Dare/SettingsViewController.m
+++ Dare/Dare/SettingsViewController.m
@@ -98,7 +98,7 @@
 -(void)backToMainPage
 {
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Storyboard" bundle:nil];
-    MainScreenViewController *mainVC = [storyboard instantiateViewControllerWithIdentifier:@"MainScreen"];    
+    MainScreenViewController *mainVC = [storyboard instantiateViewControllerWithIdentifier:@"MainScreen"];
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
@@ -119,15 +119,5 @@
     [self presentViewController:initialNavController animated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
